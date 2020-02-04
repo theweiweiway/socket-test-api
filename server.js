6b926b405f7f791c9bc80012ebfc1c4eb3282995
weiiew <weiwei.li@honeybeehub.ca>
2020-02-04 09:23:22 -0500

    recieve

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 var app = require("express")();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 
 app.use(cors());
 
-io.of("/chat").on("connection", function(socket) {
-  console.log("CONNECTED!!!");
-  //   socket.on("chat message", function(msg) {
-  //     io.emit("chat message", msg);
-  //   });
+io.of("/chat").on("connection", async function(socket) {
+  socket.emit("test", "CONNECTED TO SOCKET");
 });
 
 app.get("/", function(req, res, next) {
   res.status(200).json("hi");
 });
 
-server.listen(process.env.PORT || "3000", function() {
-  console.log("listening on *:3000");
+server.listen(port, function() {
+  console.log(`listening on ${port}`);
 });
